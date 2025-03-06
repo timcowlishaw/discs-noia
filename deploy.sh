84f6b1fee480fa29e26f1365c5373a5024b8e27c
Tim Cowlishaw <tim@timcowlishaw.co.uk>
2025-03-06 13:33:55 +0100

    font that shows interpuncts correctly, wait for deploy to finish in script

--- deploy.sh
+++ deploy.sh
@@ -4,3 +4,4 @@ docker tag discs-noia-web registry.digitalocean.com/discs-noia/discs-noia-web
 docker push registry.digitalocean.com/discs-noia/discs-noia-web
 kubectl apply -f manifest.yml
 kubectl rollout restart deployment discs-noia-deployment
+kubectl rollout status deployment discs-noia-deployment
